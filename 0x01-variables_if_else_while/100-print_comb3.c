ba5780159cbb4867134fcf789358dc89a0369945
ramla02 <ramla252017@gmail.com>
2022-12-12 07:20:53 -0800

    Inventing is a combination of brains and materials. The more brains you use, the less material you need

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints the numbers from 01 to 89, numbers separated by
+ * main - Prints the numbers from 1 to 89, numbers separated by
  * a comma followed by a space, in ascending order.
  *
  * Return: Always 0.
@@ -10,12 +10,12 @@ int main(void)
 {
 	int digit1, digit2;
 
-	for (digit1 = 01; digit1 < 10; digit1++)
+	for (digit1 = 1; digit1 < 10; digit1++)
 	{
-		for (digit2 = 01; digit2 < 10; digit2++)
+		for (digit2 = 1; digit2 < 10; digit2++)
 		{
-			putchar((digit1 % 10) + '01');
-			putchar((digit2 % 10) + '01');
+			putchar((digit1 % 10) + '1');
+			putchar((digit2 % 10) + '1');
 
 			if (digit1 == 8 && digit2 == 9)
 				continue;
