45ad81b5d2fc89671f0981ab9bb6a8c6f80d89e9
ramla02 <ramla252017@gmail.com>
2024-05-30 15:02:41 +0300

    10x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - is a function that prints the alphabet letters 10x.
+ * Description: Write a function that prints 10 times the alphabet,
+ * in lowercase, followed by a new line.
+ * You can only use _putchar twice in your code.
+ * Return: success always.
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int n;
+
+	for (n = 0; n <= 10; n++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
