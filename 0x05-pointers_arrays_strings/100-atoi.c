0916123f1ffa2a3e7cb223ec5b78e5d195e971ce
ramla02 <ramla252017@gmail.com>
2024-06-06 21:32:37 +0300

    atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _atoi - a function that convert a string to an integer.
+ * The number in the string can be preceded by an infinite number of characters
+ * You need to take into account all the - and + signs before the number
+ * If there are no numbers in the string, the function must return 0
+ * You are not allowed to use long
+ * You are not allowed to declare new variables of “type” array
+ * You are not allowed to hard-code special values
+ * We will use the -fsanitize=signed-integer-overflow gcc
+ *flag to compile your code.
+ * @s: string pointer
+ * Return: 0
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+		unsigned int num = 0;
+	do {
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+		else if (num > 0)
+			break;
+	} while (*s++);
+	return (num * sign);
+}
