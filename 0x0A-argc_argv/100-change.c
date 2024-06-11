cd5ad9340998c941d2ef8e773a31f8384ab0a421
ramla02 <ramla252017@gmail.com>
2024-06-11 09:31:27 +0300

    argument

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 /**
  *m main -  a program that prints the minimum number of coins to make change
--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 /**
  * main - a program that multiplies two numbers.
